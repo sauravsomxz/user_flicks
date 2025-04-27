b9a84daf7a0d733b7bb83921d44a88dbb479fab5
Sourav Maharana <saurav.maharana07@gmail.com>
2025-04-27 16:53:12 +0530

    Created blank AddUserView

---
+++ lib/features/add_user/view/add_user_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddUserView extends StatelessWidget {
+  const AddUserView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(automaticallyImplyLeading: true));
+  }
+}
