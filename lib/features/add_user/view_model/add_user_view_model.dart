ee00ed3ceb11476242e4e270545f6ef8a62ba6c7
Sourav Maharana <saurav.maharana07@gmail.com>
2025-04-28 00:04:59 +0530

    Added loading and bottomshet status states

--- lib/features/add_user/view_model/add_user_view_model.dart
+++ lib/features/add_user/view_model/add_user_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:user_flicks/core/app_enums.dart';
 import 'package:user_flicks/core/network/network_response.dart';
 import 'package:user_flicks/features/add_user/repository/add_user_repository.dart';
 
@@ -6,6 +7,9 @@ class AddUserViewModel extends ChangeNotifier {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController jobController = TextEditingController();
   final AddUserRepository _addUserRepository = AddUserRepository();
+
+  UserAddStatus? _status;
+  bool _isLoading = false;
 
   AddUserViewModel() {
     nameController.addListener(_onTextChanged);
@@ -18,23 +22,39 @@ class AddUserViewModel extends ChangeNotifier {
 
   Future<void> addUser() async {
     if (isFormValid) {
+      _isLoading = true;
+      notifyListeners();
+
       try {
         final String name = nameController.text;
         final String job = jobController.text;
         final response = await _addUserRepository.addUser(name: name, job: job);
 
         if (response is Success) {
-          /// TODO: HANDLE SUCCESS
+          _status = UserAddStatus.success;
         } else if (response is Failure) {
-          /// TODO: Handle error state
+          _status = UserAddStatus.failure;
         }
       } catch (e) {
-        //// HANDLE TRY
+        _status = UserAddStatus.error;
+      } finally {
+        _isLoading = false;
+        notifyListeners();
       }
     }
   }
 
   bool get isFormValid {
     return nameController.text.isNotEmpty && jobController.text.isNotEmpty;
+  }
+
+  bool get isLoading => _isLoading;
+  UserAddStatus? get status => _status;
+
+  void resetStatus() {
+    _status = null;
+    nameController.clear();
+    jobController.clear();
+    notifyListeners();
   }
 }
