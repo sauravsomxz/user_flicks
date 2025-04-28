c7358b8538ee0a94b8a3bdc34edab9b16cccb63a
Sourav Maharana <saurav.maharana07@gmail.com>
2025-04-28 23:43:00 +0530

    Added Movies Config URL and API key for global usage

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -2,12 +2,16 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
   static late final String usersBaseUrl;
-  static late final String moviesBaseUrl;
   static late final String usersAPIKey;
+  static late final String moviesBaseUrl;
+  static late final String moviesImagesBaseUrl;
+  static late final String moviesDBAPIKEY;
 
   static void init() {
     usersBaseUrl = dotenv.env['USERS_BASE_URL'] ?? '';
     moviesBaseUrl = dotenv.env['MOVIES_BASE_URL'] ?? '';
     usersAPIKey = dotenv.env['USERS_API_KEY'] ?? '';
+    moviesImagesBaseUrl = dotenv.env['MOVIES_IMAGES_BASE_URL'] ?? '';
+    moviesDBAPIKEY = dotenv.env['THE_MOVIES_DB_API_KEY'] ?? '';
   }
 }
