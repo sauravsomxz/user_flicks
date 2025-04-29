920b6f6f6e3f9ec8a3143c7f33aa1d8641b164e6
Sourav Maharana <saurav.maharana07@gmail.com>
2025-04-29 14:29:42 +0530

    Developed blank Movie Detail Screen

---
+++ lib/features/movie_details_screen/view/movie_details_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailsView extends StatelessWidget {
+  final String movieId;
+  const MovieDetailsView({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: Container());
+  }
+}
