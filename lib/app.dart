16389f3f85d803550a8a056e8b91497d73aeded1
Sourav Maharana <saurav.maharana07@gmail.com>
2025-04-29 16:50:27 +0530

    Added Proivder for Movie Details to app start

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,7 @@ import 'package:user_flicks/core/config/app_config.dart';
 import 'package:user_flicks/core/theme/app_theme.dart';
 import 'package:user_flicks/features/add_user/view_model/add_user_view_model.dart';
 import 'package:user_flicks/features/home/view_model/home_view_model.dart';
+import 'package:user_flicks/features/movie_details_screen/view_model/movie_details_vm.dart';
 import 'package:user_flicks/features/movies/view_model/list_of_movies_vm.dart';
 
 class App extends StatelessWidget {
@@ -26,6 +27,7 @@ class App extends StatelessWidget {
           create: (_) => AddUserViewModel(),
         ),
         ChangeNotifierProvider<ListOfMoviesVm>(create: (_) => ListOfMoviesVm()),
+        ChangeNotifierProvider<MovieDetailsVm>(create: (_) => MovieDetailsVm()),
       ],
       child: MaterialApp.router(
         title: 'UserFlix',
