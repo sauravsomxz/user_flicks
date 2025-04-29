c5431792204b1172cb81cdac622daa7f5d1e530a
Sourav Maharana <saurav.maharana07@gmail.com>
2025-04-29 23:51:50 +0530

    Initialized Hive Local DB and adapter for User Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:user_flicks/app.dart';
+import 'package:user_flicks/features/add_user/model/add_user_data_hive_data_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(HiveUserModelAdapter());
   await App.init();
   runApp(const App());
 }
