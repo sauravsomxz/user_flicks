9536ef380f5dfd8a2a8df5e540dba53f284e3829
Sourav Maharana <saurav.maharana07@gmail.com>
2025-04-29 17:01:01 +0530

    Section header

---
+++ lib/features/movie_details_screen/widgets/section_title.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:user_flicks/core/theme/colors.dart';
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+  const SectionTitle({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: AppColors.textPrimary,
+      ),
+    );
+  }
+}
